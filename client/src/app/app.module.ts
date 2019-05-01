0b4aad8571a1afe8cae9aa0b876ab907038cc4b7
Chris Yoo <woyoo@umich.edu>
2019-04-30 23:52:25 -0400

    still working on router-outlet inside a router-outlet

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar.component';
+import { HomeComponent } from './home.component';
 import { SpecialtyPizzaComponent } from './specialty_pizzas.component';
 import { LocationComponent } from './locations.component';
 import { BuildYourOwnComponent } from './build_your_own.component';
+import { MenuComponent } from './menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SpecialtyPizzaComponent,
-    NavbarComponent,
+    HomeComponent,
     LocationComponent,
     SpecialtyPizzaComponent,
-    BuildYourOwnComponent
+    BuildYourOwnComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'menu', component: MenuComponent },
+      { path: 'menu/buildyourown', component: BuildYourOwnComponent, outlet: 'submenu'},
+      { path: 'menu/specialtypizzas', component: SpecialtyPizzaComponent, outlet: 'submenu'},
+      { path: 'locations', component: LocationComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/locations.component.ts
+++ client/src/app/locations.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-locations',
   templateUrl: './locations.component.html',
   styleUrls: ['./locations.component.css']
 })
